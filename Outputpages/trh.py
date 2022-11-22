f9501706f8da2bc601b604b571915256cdb81332
DebayanPaul93 <114311805+DebayanPaul93@users.noreply.github.com>
2022-11-22 20:14:09 +0100

    Update trh.py

--- Outputpages/trh.py
+++ Outputpages/trh.py
@@ -1,7 +1,7 @@
 import streamlit as st,sys
 from pathlib import Path
 from PIL import Image
-import esoreader as esr, pandas as pd
+import esoreader as esr, pandas as pd, numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from pandas.plotting import register_matplotlib_converters
@@ -17,8 +17,11 @@ plt.rcParams['axes.linewidth'] = 3.0
 plt.rcParams['lines.linewidth'] = 4
 
 def trh_output():
-	original_title = '<p style="font-family:Sans serif; color:cyan; font-size: 30px;"><b>Mean Air Temperature in Conditioned Zones</b></p>'
-	st.markdown(original_title, unsafe_allow_html=True)
+	# st.expander('Mean Air Temperature in Conditioned Zones', expanded=False)
+	# original_title = '<p style="font-family:Sans serif; color:cyan; font-size: 30px;"><b>Mean Air Temperature in Conditioned Zones</b></p>'
+	# st.markdown(original_title, unsafe_allow_html=True)
+	expander = st.expander("Mean Air Temperature & Relative Humidity in Conditioned Zones", expanded=False)
+	expander.write("")
 
 	no_of_zones = 3           # find this from zone calcualtion
 	# data retrieving
@@ -32,7 +35,7 @@ def trh_output():
 	list_hourlyoutput=[]
 
 	#read eso file and convert to dataframe object
-	dd, data = esr.read(r'3_zonesnew.eso')
+	dd, data = esr.read('3_zonesnew.eso')
 	for var in list_variables:
 	    for i in range(3):
 	        frequency, key, variable = dd.find_variable(var)[i] 
@@ -43,20 +46,65 @@ def trh_output():
 	df = pd.DataFrame(list_hourlyoutput)
 	df_transposed = df.transpose()
 	df_transposed.columns = list_columnnames
-	timeindex=pd.date_range(start='01/01/2021/00:00:00', end='31/12/2021/23:00:00',freq='H')
+	timeindex = pd.date_range(start='01/01/2021/00:00:00', end='31/12/2021/23:00:00',freq='H')
 	df_transposed.index = timeindex
-	df_transposed_zone1 = df_transposed[['Zone 1 T','Zone 1 RH']]
-	df_transposed_zone2 = df_transposed[['Zone 2 T','Zone 2 RH']]
-	df_transposed_zone3 = df_transposed[['Zone 3 T','Zone 3 RH']]
+	list_highTavg, list_highRHavg = [],[]
+
+	if(no_of_zones == 1):
+		df_transposed_zone1 = df_transposed[['Zone 1 T','Zone 1 RH']]
+		highTavg1=round((len(df_transposed_zone1[df_transposed_zone1['Zone 1 T'] > 18.1])/8760 * 100),1)               #0.1 more than annual avergae T setpoint, need to read
+		list_highTavg1.append(highTavg)
+		highRHavg1=round((len(df_transposed_zone1[df_transposed_zone1['Zone 1 RH'] > 50.1])/8760 * 100),1)              #0.1 more than annual avergae RH setpoint, need to read
+		list_highRHavg.append(highRHavg1)
+
+
+	elif(no_of_zones == 2):
+		df_transposed_zone1 = df_transposed[['Zone 1 T','Zone 1 RH']]
+		df_transposed_zone2 = df_transposed[['Zone 2 T','Zone 2 RH']]
+		highTavg1=round((len(df_transposed_zone1[df_transposed_zone1['Zone 1 T'] > 18.1])/8760 * 100),1)
+		highTavg2=round((len(df_transposed_zone1[df_transposed_zone2['Zone 2 T'] > 18.1])/8760 * 100),1)
+		list_highTavg.append(highTavg1)
+		list_highTavg.append(highTavg2)
+		highRHavg1=round((len(df_transposed_zone1[df_transposed_zone1['Zone 1 RH'] > 50.1])/8760 * 100),1) 
+		highRHavg2=round((len(df_transposed_zone1[df_transposed_zone2['Zone 2 RH'] > 50.1])/8760 * 100),1) 
+		list_highRHavg.append(highRHavg1)
+		list_highRHavg.append(highRHavg2)
+
+	else:
+		df_transposed_zone1 = df_transposed[['Zone 1 T','Zone 1 RH']]
+		df_transposed_zone2 = df_transposed[['Zone 2 T','Zone 2 RH']]
+		df_transposed_zone3 = df_transposed[['Zone 3 T','Zone 3 RH']]
+		highTavg1=round((len(df_transposed_zone1[df_transposed_zone1['Zone 1 T'] > 18.1])/8760 * 100),1) 
+		highTavg2=round((len(df_transposed_zone1[df_transposed_zone2['Zone 2 T'] > 18.1])/8760 * 100),1) 
+		highTavg3=round((len(df_transposed_zone1[df_transposed_zone3['Zone 3 T'] > 18.1])/8760 * 100),1) 
+		list_highTavg.append(highTavg1)
+		list_highTavg.append(highTavg2)
+		list_highTavg.append(highTavg3)
+		highRHavg1=round((len(df_transposed_zone1[df_transposed_zone1['Zone 1 RH'] > 50.1])/8760 * 100),1) 
+		highRHavg2=round((len(df_transposed_zone1[df_transposed_zone2['Zone 2 RH'] > 50.1])/8760 * 100),1) 
+		highRHavg3=round((len(df_transposed_zone1[df_transposed_zone3['Zone 3 RH'] > 50.1])/8760 * 100),1) 
+		list_highRHavg.append(highRHavg1)
+		list_highRHavg.append(highRHavg2)
+		list_highRHavg.append(highRHavg3)
+
 
 	#check if already file exists (Simulation is done)
 	if (Path('Temperature.jpg').is_file() is False):        
 		#T Plot creater
 		fig1 = plt.figure(figsize = (24,12))
 		axes = fig1.add_subplot(111)
-		axes.plot(df_transposed_zone1.index, df_transposed_zone1['Zone 1 T'], color="green", linestyle='solid', label='')
-		axes.plot(df_transposed_zone2.index, df_transposed_zone2['Zone 2 T'], color="orange", linestyle='dashed', label='')
-		axes.plot(df_transposed_zone3.index, df_transposed_zone3['Zone 3 T'], color="blue", linestyle='dotted', label='')
+		if(no_of_zones == 1):
+			axes.plot(df_transposed_zone1.index, df_transposed_zone1['Zone 1 T'], color="green", linestyle='solid', label='')
+		elif(no_of_zones == 2):
+			axes.plot(df_transposed_zone1.index, df_transposed_zone1['Zone 1 T'], color="green", linestyle='solid', label='')
+			axes.plot(df_transposed_zone2.index, df_transposed_zone2['Zone 2 T'], color="orange", linestyle='dashed', label='')
+			axes.legend(['Ground Floor', '$2^{nd}$ Floor'], fontsize=24, frameon=False)
+		else:
+			axes.plot(df_transposed_zone1.index, df_transposed_zone1['Zone 1 T'], color="green", linestyle='solid', label='')
+			axes.plot(df_transposed_zone2.index, df_transposed_zone2['Zone 2 T'], color="orange", linestyle='dashed', label='')
+			axes.plot(df_transposed_zone3.index, df_transposed_zone3['Zone 3 T'], color="blue", linestyle='dotted', label='')
+			axes.legend(['Ground Floor', '$2^{nd}$ Floor', '$3^{rd}$ Floor'], fontsize=24, frameon=False)
+
 		axes.grid(axis = 'y')
 		axes.grid(axis = 'x')
 		axes.set_ylabel('Temperature [°C]', fontsize = 30, labelpad=20)
@@ -64,7 +112,6 @@ def trh_output():
 		fig1.tight_layout()
 		axes.autoscale(enable=True)
 		axes.set_xticks(df_transposed_zone1.index, minor = True)
-		axes.legend(['Ground Floor', '$2^{nd}$ Floor', '$3^{rd}$ Floor'], fontsize=24, frameon=False)
 
 		locator = mdates.MonthLocator()
 		myFmt = mdates.DateFormatter('%b')
@@ -76,7 +123,7 @@ def trh_output():
 		      tick.label.set_fontsize(20) 
 		for tick in axes.yaxis.get_major_ticks():
 		      tick.label.set_fontsize(20)
-
+		        
 		plt.ylim(10,25)
 		plt.text(0.50, 0.16, 'Lower Temperature Limit',verticalalignment='bottom', horizontalalignment='right',transform=axes.transAxes, color='red', fontsize=25)
 		plt.text(0.25, 0.82, 'Upper Temperature Limit',verticalalignment='bottom', horizontalalignment='right',transform=axes.transAxes, color='red', fontsize=25)
@@ -88,24 +135,34 @@ def trh_output():
 
 	#plot through streamlit
 	timg = Image.open("Temperature.jpg")
-	st.image(timg, width=None)
+	#expander.image(timg)
+	#st.image(timg, width=None)
 		
 
 
-	for i in range(3):
-		st.write("")
+	# for i in range(3):
+	# 	st.write("")
 
-	original_title = '<p style="font-family:Sans serif; color:cyan; font-size: 30px;"><b>Mean Air Relative Humidity in Conditioned Zones</b></p>'
-	st.markdown(original_title, unsafe_allow_html=True)
+	# original_title = '<p style="font-family:Sans serif; color:cyan; font-size: 30px;"><b>Mean Air Relative Humidity in Conditioned Zones</b></p>'
+	# st.markdown(original_title, unsafe_allow_html=True)
 
 	#check if already file exists (Simulation is done)
-	if (Path('RH.jpg').is_file() is False):  
+	if (Path('RH.jpg').is_file() is False):                
 		#RH Plot creater
 		fig2 = plt.figure(figsize = (24,12))
 		axes1 = fig2.add_subplot(111)
-		axes1.plot(df_transposed_zone1.index, df_transposed_zone1['Zone 1 RH'], color="green", linestyle='solid', label='')
-		axes1.plot(df_transposed_zone2.index, df_transposed_zone2['Zone 2 RH'], color="orange", linestyle='dashed', label='')
-		axes1.plot(df_transposed_zone3.index, df_transposed_zone3['Zone 3 RH'], color="blue", linestyle='dotted', label='')
+		if(no_of_zones == 1):
+			axes1.plot(df_transposed_zone1.index, df_transposed_zone1['Zone 1 RH'], color="green", linestyle='solid', label='')
+		elif(no_of_zones == 2):
+			axes1.plot(df_transposed_zone1.index, df_transposed_zone1['Zone 1 RH'], color="green", linestyle='solid', label='')
+			axes1.plot(df_transposed_zone2.index, df_transposed_zone2['Zone 2 RH'], color="orange", linestyle='dashed', label='')
+			axes1.legend(['Ground Floor', '$2^{nd}$ Floor'], fontsize=24, frameon=False)
+		else:
+			axes1.plot(df_transposed_zone1.index, df_transposed_zone1['Zone 1 RH'], color="green", linestyle='solid', label='')
+			axes1.plot(df_transposed_zone2.index, df_transposed_zone2['Zone 2 RH'], color="orange", linestyle='dashed', label='')
+			axes1.plot(df_transposed_zone3.index, df_transposed_zone3['Zone 3 RH'], color="blue", linestyle='dotted', label='')
+			axes1.legend(['Ground Floor', '$2^{nd}$ Floor', '$3^{rd}$ Floor'], fontsize=24, frameon=False)
+		
 		axes1.grid(axis = 'y')
 		axes1.grid(axis = 'x')
 		axes1.set_ylabel('Relative Humidity [%]', fontsize = 30, labelpad=20)
@@ -113,7 +170,6 @@ def trh_output():
 		fig2.tight_layout()
 		axes1.autoscale(enable=True)
 		axes1.set_xticks(df_transposed_zone1.index, minor = True)
-		axes1.legend(['Ground Floor', '$2^{nd}$ Floor', '$3^{rd}$ Floor'], fontsize=24, frameon=False)
 
 		locator = mdates.MonthLocator()
 		myFmt = mdates.DateFormatter('%b')
@@ -125,7 +181,7 @@ def trh_output():
 		      tick.label.set_fontsize(20) 
 		for tick in axes1.yaxis.get_major_ticks():
 		      tick.label.set_fontsize(20)
-
+		        
 		plt.ylim(30,70)
 		plt.text(0.44, 0.08, 'Lower RH Limit',verticalalignment='bottom', horizontalalignment='right',transform=axes1.transAxes, color='red', fontsize=25)
 		plt.text(0.25, 0.78, 'Upper RH Limit',verticalalignment='bottom', horizontalalignment='right',transform=axes1.transAxes, color='red', fontsize=25)
@@ -137,4 +193,125 @@ def trh_output():
 
 	#plot through streamlit
 	rhimg = Image.open("RH.jpg")
-	st.image(rhimg, width=None)
+	#st.image(rhimg, width=None)
+
+	col_T, col_RH = st.columns(2)
+	
+	with col_T:
+		expander1 = st.expander("Temperature", expanded=False)
+		expander1.image(timg)
+	with col_RH:
+		expander2 = st.expander("Relative Humidity", expanded=False)
+		expander2.image(rhimg)
+
+	expander = st.expander("Yearly Number of Hours above Annual Average Temperature Setpoint", expanded=False)
+	expander.write("")
+
+	if(no_of_zones == 1):
+		col1, col2, col3 = st.columns(3)
+		with col1:
+			st.write("")
+		with col2:
+			expander2 = st.expander("Ground Floor", expanded=False)
+			expander2.metric(label="", value=str(list_highTavg[0])+ "%")
+		with col3:
+			st.write("")
+
+		expander = st.expander("Yearly Number of Hours above Annual Average Relative Humidity Setpoint", expanded=False)
+		expander.write("")
+
+		col1, col2, col3 = st.columns(3)
+		with col1:
+			st.write("")
+		with col2:
+			expander2 = st.expander("Ground Floor", expanded=False)
+			expander2.metric(label="", value=str(list_highRHavg[0])+ "%")
+		with col3:
+			st.write("")
+
+	elif(no_of_zones == 2):
+		col1, col2 = st.columns(2)
+		with col1:
+			expander1 = st.expander("Ground Floor", expanded=False)
+			expander1.metric(label="", value=str(list_highTavg[0])+ "%")
+		with col2:
+			expander2 = st.expander("2nd Floor", expanded=False)
+			deltaT1 = list_highTavg[1] - list_highTavg[0]
+			deltaT1 = round(deltaT1,1)
+			if(deltaT1<0):
+				expander2.metric(label="", value=str(list_highTavg[1])+ "%", delta=deltaT1, delta_color="inverse")
+			else:
+				expander2.metric(label="", value=str(list_highTavg[1])+ "%", delta=deltaT1)
+
+		expander = st.expander("Yearly Number of Hours above Annual Average Relative Humidity Setpoint", expanded=False)
+		expander.write("")
+
+		col1, col2 = st.columns(2)
+		with col1:
+			expander1 = st.expander("Ground Floor", expanded=False)
+			expander1.metric(label="", value=str(list_highRHavg[0])+ "%")
+		with col2:
+			expander2 = st.expander("2nd Floor", expanded=False)
+			deltaRH1 = list_highRHavg[1] - list_highRHavg[0]
+			deltaRH1 = round(deltaRH1,1)
+			if(deltaRH1<0):
+				expander2.metric(label="", value=str(list_highRHavg[1])+ "%", delta=deltaRH1, delta_color="inverse")
+			else:
+				expander2.metric(label="", value=str(list_highRHavg[1])+ "%", delta=deltaRH1)
+
+	else:
+		col1, col2, col3 = st.columns(3)
+		with col1:
+			expander1 = st.expander("Ground Floor", expanded=False)
+			expander1.metric(label="", value=str(list_highTavg[0])+ "%", delta="-", delta_color="off")
+		with col2:
+			expander2 = st.expander("2nd Floor", expanded=False)
+			deltaT1 = list_highTavg[1] - list_highTavg[0]
+			deltaT1 = round(deltaT1,1)
+			if(deltaT1<0):
+				expander2.metric(label="", value=str(list_highTavg[1])+ "%", delta=deltaT1, delta_color="inverse")
+			else:
+				expander2.metric(label="", value=str(list_highTavg[1])+ "%", delta=deltaT1)
+		with col3:
+			expander3 = st.expander("3rd Floor", expanded=False)
+			deltaT2 = list_highTavg[2] - list_highTavg[1]
+			deltaT2 = round(deltaT2,1)
+			if(deltaT2<0):	
+				expander3.metric(label="", value=str(list_highTavg[2])+ "%", delta=deltaT2, delta_color="inverse")
+			else:
+				expander3.metric(label="", value=str(list_highTavg[2])+ "%", delta=deltaT2)
+
+		expander = st.expander("Yearly Number of Hours above Annual Average Relative Humidity Setpoint", expanded=False)
+		expander.write("")
+
+		col1, col2, col3 = st.columns(3)
+		with col1:
+			expander1 = st.expander("Ground Floor", expanded=False)
+			expander1.metric(label="", value=str(list_highRHavg[0])+ "%", delta="-", delta_color="off")
+		with col2:
+			expander2 = st.expander("2nd Floor", expanded=False)
+			deltaRH1 = list_highRHavg[1] - list_highRHavg[0]
+			deltaRH1 = round(deltaRH1,1)
+			if(deltaRH1<0):
+				expander2.metric(label="", value=str(list_highRHavg[1])+ "%", delta=deltaRH1, delta_color="inverse")
+			else:
+				expander2.metric(label="", value=str(list_highRHavg[1])+ "%", delta=deltaRH1)
+		with col3:
+			expander3 = st.expander("3rd Floor", expanded=False)
+			deltaRH2 = list_highRHavg[2] - list_highRHavg[1]
+			deltaRH2 = round(deltaRH2,1)
+			if(deltaRH2<0):	
+				expander3.metric(label="", value=str(list_highRHavg[2])+ "%", delta=deltaRH2, delta_color="inverse")
+			else:
+				expander3.metric(label="", value=str(list_highRHavg[2])+ "%", delta=deltaRH2)
+
+
+
+
+
+
+
+
+
+
+	
