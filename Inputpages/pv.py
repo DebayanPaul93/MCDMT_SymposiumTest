a7fa68a64546ce7e93d396e8605869b07803a5c5
DebayanPaul93 <114311805+DebayanPaul93@users.noreply.github.com>
2022-11-22 22:33:52 +0100

    Update pv.py

--- Inputpages/pv.py
+++ Inputpages/pv.py
@@ -1,6 +1,8 @@
 #import packages
 import streamlit as st, time
 
+inputconfirm = False
+
 #user input
 
 def pv_input():
@@ -43,27 +45,44 @@ def pv_input():
 	for i in range(3):
 		st.write("")
 
-	# calbutton = st.button("Calculate", key='simlcalbutton')
-	# if(calbutton):
-	# 	with st.spinner('Wait for the Results...'):
-	# 		time.sleep(5)
-	# 	st.success('Done!')
+	#Styling the Button
+	button_style = """
+	        <style>
+	        .stButton > button {
+	            color: black;
+	            background: white;
+	            width: 150px;
+	            height: 50px;
+	        }
+	        </style>
+	        """
+	st.markdown(button_style, unsafe_allow_html=True)
+
+	alloptionselection_check = st.checkbox('Confirm all necessary inputs have been selected',key='select')
+	if(alloptionselection_check):
+		calbutton = st.button("Calculate", key='simlcalbutton')
+		if(calbutton):
+			with st.spinner('Wait for the Results...'):
+				time.sleep(5)
+			st.success('Simulations Successfully Done!')
+			st.success('Results are being prepared for visualization...')
+			time.sleep(2) # Just to add a delay to show up visualization
+
+		global inputconfirm 
+		inputconfirm = alloptionselection_check
 
 
-	# for i in range(5):
-	# 	st.write("")
-	# resultmenu = ['','Conditioned Space T & RH','Utility Consumption','Collection Lifetime','PV Performance']           #added a blank selection field
-	# resultchoice = st.sidebar.selectbox('Select Result Page',resultmenu)
-
-	# match resultchoice:
-	#     case 'Conditioned Space T & RH': Outputpages.trh.trh_output()
-	#     case 'Utility Consumption': Outputpages.energy.energy_output()
-	#     case 'Collection Lifetime': Outputpages.paperlife.paperlife_output()
-	#     case 'PV Performance': Outputpages.pvresults.pv_output()
+    
 
 
 
-	# st.markdown("### Update list of solar panels!!")
+
+
+   	
+   	
+   		
+
+   	
 
 
     
