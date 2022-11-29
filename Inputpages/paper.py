9178ff145c202f9dd75e96f90fa0fdd93c5a6f1b
DebayanPaul93 <114311805+DebayanPaul93@users.noreply.github.com>
2022-11-29 22:00:05 +0100

    Update paper.py

--- Inputpages/paper.py
+++ Inputpages/paper.py
@@ -3,7 +3,7 @@ import streamlit as st
 #user input
 
 def paper_input():
-	list_pH,list_intdp,list_percent=[],[],[]
+	list_pH,list_intdp=[],[]
 	original_title = '<p style="font-family:Sans serif; color:Cyan; font-size: 30px;"><b>Number of Collection Objects</b></p>'
 	st.markdown(original_title, unsafe_allow_html=True)
 	col1, col2, col3, col4 = st.columns(4)
@@ -12,18 +12,14 @@ def paper_input():
 	for counter in range (objectno):
 		if(objectno>1):
 			st.markdown("#### Type {} Object Chemical Properties".format(counter+1))
-			#if(objectno == 2):
-			colc, cold, cole = st.columns(3)
+			colc, cold = st.columns(2)
 			with colc:
 				pH = st.slider("Select pH level",min_value=3,max_value=9,value=8,step=1,key='ph{}'.format(counter+1))
 			with cold:
 				int_dp = st.slider("Select Initial DP",min_value=500,max_value=2500,value=1200,step=50,key='dp{}'.format(counter+1))
-			with cole:
-				col_perct = st.slider("Select % of Object {} in Collection".format(counter+1),min_value=10,max_value=100,step=10,key='percent{}'.format(counter+1))
 
 			list_pH.append(pH)
 			list_intdp.append(int_dp)
-			list_percent.append(col_perct)
 
 		else:
 			st.markdown("#### Object Chemical Properties")
@@ -32,14 +28,8 @@ def paper_input():
 				pH = st.slider("Select pH level",min_value=3,max_value=9,value=8,step=1,key='ph1')
 			with colb:
 				int_dp = st.slider("Select Initial DP",min_value=500,max_value=2500,value=1200,step=50,key='dp1')
-			col_perct = 100
 			list_pH.append(pH)
 			list_intdp.append(int_dp)
-			list_percent.append(col_perct)
-
-	#Sum check 
-	if(sum(list_percent)!=100):
-		st.warning("The sum of all percentages should be 100 in case of multiple objects")
 
 	original_title = '<p style="font-family:Sans serif; color:Cyan; font-size: 30px;"><b>Critical DP of Collection</b></p>'
 	st.markdown(original_title, unsafe_allow_html=True)
