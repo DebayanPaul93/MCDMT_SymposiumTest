3c8a69932d22cdff2943afc8af92b8277a8e7932
DebayanPaul93 <114311805+DebayanPaul93@users.noreply.github.com>
2022-11-30 18:30:44 +0100

    Update energy.py

--- Outputpages/energy.py
+++ Outputpages/energy.py
@@ -28,34 +28,75 @@ def energy_output():
 	areadf_table.set_index('Unnamed: 0', inplace=True)
 	area = round(areadf_table.loc['Net Conditioned Building Area']['Area [m2]'])
 
-	enduse_table = pd.read_html('eplustbl.htm',
-               match='Heating', header=0)[1]
+	# enduse_table = pd.read_html('eplustbl.htm',
+ #               match='Heating', header=0)[1]
 
-	enduse_table.set_index('Unnamed: 0', inplace=True)
-	typeofuselist = ['Heating','Cooling','Fans','Pumps','Humidification']
-	valueslist = []
-	for use in typeofuselist:
-	    valueslist.append(round(enduse_table.loc[use]['Electricity [kWh]'] / area,2))
+	# enduse_table.set_index('Unnamed: 0', inplace=True)
+	# typeofuselist = ['Heating','Cooling','Fans','Pumps','Humidification']
+	# valueslist = []
+	# for use in typeofuselist:
+	#     valueslist.append(round(enduse_table.loc[use]['Electricity [kWh]'] / area,2))
 	    
-	usevaluedf = pd.DataFrame(valueslist,columns=['Consumption (kWh/m2)'],index=typeofuselist)
-	misc = usevaluedf.loc[['Fans', 'Pumps']].sum()[0]
-	valueslist.append(misc)
-	del valueslist[2]
-	del valueslist[2]
+	# usevaluedf = pd.DataFrame(valueslist,columns=['Consumption (kWh/m2)'],index=typeofuselist)
+	# misc = usevaluedf.loc[['Fans', 'Pumps']].sum()[0]
+	# valueslist.append(misc)
+	# del valueslist[2]
+	# del valueslist[2]
+	#usevaluedf.drop(usevaluedf.index[[2,3]], inplace=True)
+	#read the eso file for breakdown
+	dd, data = esr.read('EnergyConsumption.eso')
+	# monthly data (boiler, cooling coil, pumps, and fans)
+	Mdata = pd.DataFrame()
+	for nr in dd.variables:
+	    if dd.variables[nr][0] == "Monthly":
+	        if dd.variables[nr][2] == "Fan Electric Energy" or dd.variables[nr][2] == "Cooling Coil Total Cooling Energy" or dd.variables[nr][2] == "Boiler Electric Energy" or dd.variables[nr][2] == "Pump Electric Energy" or dd.variables[nr][2] == "Humidifier Electric Energy":
+	            Mdata[nr] = data[nr]
+	Mdata_kWh = Mdata/3600000
+	        
+	month_index = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul','Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+	Mdata_kWh.index = month_index
 
+	# 1- rename the .eso names of columns
+	col_dict = { 243: 'Supply fan', 292 : 'Extract fan', 406 : 'Humidifier', 419 : 'Cooling Coil', 470 : 'Boiler', 520 : 'Pumps', }   ## key→old name, value→new name
+	Mdata_kWh.columns = [col_dict.get(x, x) for x in Mdata_kWh.columns]
+	Mdata_kWh['Fans and Pumps'] = Mdata_kWh['Supply fan'] + Mdata_kWh['Extract fan'] + Mdata_kWh['Pumps']
+	Mdata_kWh_new = Mdata_kWh.drop(['Supply fan', 'Extract fan', 'Pumps' ], axis=1)
+	# 4- reorder the data so that boiler comes first
+	Mdata_kWh_new = Mdata_kWh_new[['Boiler', 'Cooling Coil','Humidifier','Fans and Pumps']]
+	Mdata_kWh_new_normalized = Mdata_kWh_new/area
+
+	#Change legends for table and plot
 	typeofuselist = ['Heating','Cooling','Humidification','Miscellaneous (Fans & Pumps)']
-	usevaluedf.drop(usevaluedf.index[[2,3]], inplace=True)
+	valueslist = Mdata_kWh_new_normalized.sum(axis=0).tolist()
 
 	#recreate the dataframe
 	usevaluedf = pd.DataFrame(valueslist,columns=['Consumption (kWh/m2)'],index=typeofuselist)
-	totalutilityconsumption = round(usevaluedf['Consumption (kWh/m2)'].sum(),2)
 
-	if (Path('EndUse.jpg').is_file() is False):                
+	#Display the dataframe as table
+	col1, col2, col3 = st.columns(3)
+	with col1:
+		st.write("")
+	with col2:
+		st.dataframe(usevaluedf, width=400)
+	with col3:
+		st.write("")
+
+	if (Path('EndUseConsumption.jpg').is_file() is False):                
 		#End-Use Plot creater
-		fig = plt.figure(figsize = (24,12))
-		ax = fig.add_axes([0,0,1,1])
-		usetypes = usevaluedf.index.tolist()
-		ax.bar(usetypes,valueslist)
+		fig, ax = plt.subplots(figsize = (24,12))
+		width = 0.45       # the width of the bars: can also be len(x) sequence
+		xlabels = Mdata_kWh_new_normalized.index.tolist()
+		boiler = Mdata_kWh_new_normalized['Boiler']
+		cooling_coil = Mdata_kWh_new_normalized['Cooling Coil']
+		humidifier = Mdata_kWh_new_normalized['Humidifier']
+		misc = Mdata_kWh_new_normalized['Fans and Pumps']
+
+
+		ax.bar(xlabels, boiler, width, color = 'red')
+		ax.bar(xlabels, cooling_coil, width, bottom = boiler, color = 'blue')
+		ax.bar(xlabels, humidifier, width, bottom = boiler+cooling_coil, color = 'cyan')
+		ax.bar(xlabels, misc, width, bottom = boiler+cooling_coil+humidifier, color = 'gold')
+
 		ax.grid(axis = 'y')
 		ax.grid(axis = 'x')
 		ax.set_ylabel('End-Use Consumption (kWh/$m^2$)', fontsize = 30, labelpad=20)
@@ -74,25 +115,16 @@ def energy_output():
 
 		for tick in ax.yaxis.get_major_ticks():
 		      tick.label.set_fontsize(25)
-		        
+
 		for tick in ax.xaxis.get_major_ticks():
 		      tick.label.set_fontsize(25)
-		        
-		plt.legend(['Total End-use Consumption (kWh/$m^2$): '+str(totalutilityconsumption)], fontsize = 25,loc='best', frameon=False, ncol=1, bbox_to_anchor=(1.0, 1.0))
-		plt.savefig('EndUse.jpg', bbox_inches='tight', pad_inches=0.1)
+
+		plt.legend(['Heating', 'Cooling', 'Humidification', 'Miscellaneous (Fans & Pumps)'], fontsize = 25,loc='best', frameon=False, ncol=1, bbox_to_anchor=(1.0, 1.0))
+		plt.savefig('EndUseConsumption.jpg', bbox_inches='tight', pad_inches=0.1)
 
 	#plot through streamlit
-	enduseimg = Image.open("EndUse.jpg")
+	enduseimg = Image.open("EndUseConsumption.jpg")
 	expander.image(enduseimg)
-
-	#Display the dataframe as table
-	col1, col2, col3 = st.columns(3)
-	with col1:
-		st.write("")
-	with col2:
-		st.dataframe(usevaluedf, width=400)
-	with col3:
-		st.write("")
 
 
 	expander1 = st.expander("Heating & Cooling Source Energy Consumption (Monthly Report)", expanded=False)
