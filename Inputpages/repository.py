fa546c16c71131f8d275d6ddf0bbfe3a7b640bde
DebayanPaul93 <114311805+DebayanPaul93@users.noreply.github.com>
2022-12-09 08:55:19 +0100

    Update repository.py

--- Inputpages/repository.py
+++ Inputpages/repository.py
@@ -1,32 +1,33 @@
 import streamlit as st,pandas as pd,os, math
+import streamlit_ext as ste
 
 #user input
 
 def size_input():
-	original_title = '<p style="font-family:Sans serif; color:Magenta; font-size: 30px;"><b>Collection Amount</b></p>'
+	original_title = '<p style="font-family:Sans serif; color:White; font-size: 30px;"><b>Collection Amount</b></p>'
 	st.markdown(original_title, unsafe_allow_html=True)
 	col1, col2, col3 = st.columns(3)
 	with col1:
-		collectionsize = st.number_input("Enter Size of Collection (in km)",min_value=5,max_value=200,step=5)
-	original_title = '<p style="font-family:Sans serif; color:Magenta; font-size: 30px;"><b>Storage Filling Type</b></p>'
+		collectionsize = ste.number_input("Enter Size of Collection (in km)",min_value=5,max_value=200,value=15,step=5, key='collectionsize')
+	original_title = '<p style="font-family:Sans serif; color:White; font-size: 30px;"><b>Storage Filling Type</b></p>'
 	st.markdown(original_title, unsafe_allow_html=True)
-	fillperct = st.radio("Select filling percentage",('Fixed rack (35%)','Mobile rack (80%)','Custom Filling Percentage'))
+	fillperct = ste.radio("Select filling percentage",('Mobile rack (80%)','Fixed rack (35%)','Custom Filling Percentage'))
 	if(fillperct == 'Custom Filling Percentage'):
 		colp, colq, colr = st.columns(3)
 		with colp:
-			fillperctvalue = st.number_input("Enter Filling Percentage",min_value=10,max_value=80,step=5)
+			fillperctvalue = ste.number_input("Enter Filling Percentage",min_value=10,max_value=80,value=50,step=5,key='customfillpercentage')
 	else:
 		fillperctvalue = int(fillperct[-4:-2])
 
-	original_title = '<p style="font-family:Sans serif; color:Magenta; font-size: 30px;"><b>Repository Dimension</b></p>'
+	original_title = '<p style="font-family:Sans serif; color:White; font-size: 30px;"><b>Repository Dimension</b></p>'
 	st.markdown(original_title, unsafe_allow_html=True)
 	col5, col6, col7 = st.columns(3)
 	with col5:
-		repo_length = st.number_input("Enter Length of Repository (in m)",min_value=5,max_value=100,value=15,step=5,key='length')
+		repo_length = ste.number_input("Enter Length of Repository (in m)",min_value=5,max_value=100,value=15,step=5,key='length')
 	with col6:
-		repo_width = st.number_input("Enter Width of Repository (in m)",min_value=5,max_value=100,value=10,step=5,key='width')
+		repo_width = ste.number_input("Enter Width of Repository (in m)",min_value=5,max_value=100,value=10,step=5,key='width')
 	with col7:
-		repo_floorheight = st.number_input("Enter Height of each Floor (in m)",min_value=3.5,max_value=6.0,value=4.0,step=0.5,key='height',format="%.1f")
+		repo_floorheight = ste.number_input("Enter Height of each Floor (in m)",min_value=3.5,max_value=6.0,value=4.0,step=0.5,key='height',format="%.1f")
 
 	collection_volume = collectionsize*1000*0.4*0.26
 	building_volume = (collection_volume*100)/fillperctvalue
@@ -36,18 +37,10 @@ def size_input():
 	if(number_of_floors > 3):
 		st.error("Calculated number of floors for the archive is higher than 3. Please change length/width or filling type of repository to make the budiling of max 3 floors", icon="⚠️")
 
-# 	valuelist = [collectionsize,fillperctvalue,repo_length,repo_width,repo_floorheight, number_of_floors]
-# 	columnlist = ['Collection Size (km)', 'Filling Percentage (%)', 'Repository Length (m)', 'Repository Width (m)', 'Repository Floor Height (m)', 'Number of Floors']
+	for i in range(3):
+		st.write("")
 
-
-# 	#Current working directory
-# 	cwd = os.getcwd()
-# 	#set path to subdirectory and inputfilename
-# 	inputfilesnamewithpath = cwd+'\\Inputfiles\\RepositoryUserInputs.csv'
-
-
-# 	#Write the inputs to a csv file
-# 	inputdf = pd.DataFrame([valuelist], columns = columnlist)
-# 	inputdf.to_csv(inputfilesnamewithpath, index=False)
+	if(number_of_floors <= 3):
+		save = st.checkbox('Save Inputs (*)', value=False, key='repositorysave')
 	
 
