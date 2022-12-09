a94153c62c54cb86360c3c8a207cf2e10b301b08
DebayanPaul93 <114311805+DebayanPaul93@users.noreply.github.com>
2022-12-09 08:51:21 +0100

    Update weather.py

--- Inputpages/weather.py
+++ Inputpages/weather.py
@@ -5,7 +5,7 @@ refweatheryear = 2018
 def weather_input():
 	
 	scenario_list = ['NEN 5060:2018','NEN 5060:2018 (1% Extreme)', 'IPCC A1B', 'IPCC A2', 'IPCC B1']
-	original_title = '<p style="font-family:Sans serif; color:Red; font-size: 30px;"><b>Location of Archive</b></p>'
+	original_title = '<p style="font-family:Sans serif; color:White; font-size: 30px;"><b>Location of Archive</b></p>'
 	st.markdown(original_title, unsafe_allow_html=True)
 
 	location = st.radio("Select a location",('Netherlands','Netherlands Antilles'),help='Physical Location of the archive')
@@ -16,7 +16,7 @@ def weather_input():
 		with col1:
 			refscenario = st.selectbox("Select a weather scenario", scenario_list,help='Whole Building Energy Simualtion are dependant on climatic data of the location. Based on the selected location current Dutch reference weather data or IPCC-based future weather data can be used')
 		if(refscenario in scenario_list[2:]):
-			original_title = '<p style="font-family:Sans serif; color:Red; font-size: 20px;"><b>IPCC Future Reference Year</b></p>'
+			original_title = '<p style="font-family:Sans serif; color:White; font-size: 20px;"><b>IPCC Future Reference Year</b></p>'
 			st.markdown(original_title, unsafe_allow_html=True)
 			ipccyear = st.radio("Select a reference future year",('2050','2100'), horizontal=True)
 			global refweatheryear 
