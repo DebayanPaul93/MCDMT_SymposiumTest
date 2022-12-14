fdd20aaf858099c3abfc110ae48c4aa6b0d5730e
DebayanPaul93 <114311805+DebayanPaul93@users.noreply.github.com>
2022-12-14 22:49:10 +0100

    Update weather.py

--- Inputpages/weather.py
+++ Inputpages/weather.py
@@ -1,12 +1,11 @@
 import streamlit as st,pandas as pd,os, math
 import streamlit_ext as ste
 
-refweatheryear = 2018   #Default value
-
 #user input
 def weather_input():
 	
 	scenario_list = ['NEN 5060:2018','NEN 5060:2018 (1% Extreme)', 'NEN 5060:2018 (5% Extreme)', 'IPCC A1B', 'IPCC A2', 'IPCC B1']
+	ipccyear_list = [2050,2100]
 	
 	original_title = '<p style="font-family:Sans serif; color:White; font-size: 30px;"><b>Weather Scenario</b></p>'
 	st.markdown(original_title, unsafe_allow_html=True)
@@ -16,9 +15,11 @@ def weather_input():
 	if(refscenario in ['IPCC A1B', 'IPCC A2', 'IPCC B1']):
 		original_title = '<p style="font-family:Sans serif; color:White; font-size: 20px;"><b>IPCC Future Reference Year</b></p>'
 		st.markdown(original_title, unsafe_allow_html=True)
-		ipccyear = ste.radio("Select a reference future year",('2050','2100'), horizontal=True, key='ipccyear')
-		global refweatheryear 
+		ipccyear = ste.radio("Select a reference future year", ipccyear_list, horizontal=True, key='ipcc') 
 		refweatheryear = ipccyear
+
+	elif(refscenario in ['NEN 5060:2018','NEN 5060:2018 (1% Extreme)', 'NEN 5060:2018 (5% Extreme)']):
+		refweatheryear = 2018
 
 	for i in range(3):
 		st.write("")
@@ -38,6 +39,5 @@ def weather_input():
 		inputdf = pd.DataFrame([valuelist], columns = columnlist)
 
 		inputdf.to_csv(inputfilesnamewithpath, index=False)
-		
 
 
